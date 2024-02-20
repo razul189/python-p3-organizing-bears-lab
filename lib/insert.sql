21053389e78e069357e42b47a70f13442ba3169d
Ramia Zulfiqar <razulfiqar1@gmail.com>
2024-02-20 16:48:51 -0500

    lab complete

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES("Mr. Chocolate", 3, "M", "brown", "nice", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES("Rowdy", 4, "M", "brown", "mean", 0);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES("Tabitha", 5, "F", "brown", "fun", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES("Sergeant Brown", 6, "M", "brown", "serious", 0);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES("Melissa", 7, "F", "brown", "silly", 0);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES("Grinch", 8, "M", "brown", "grumpy", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES("Wendy", 9, "F", "brown", "nice", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES( NULL, 10, "M", "brown", "loving", 0);
+
+
